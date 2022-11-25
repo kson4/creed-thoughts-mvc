850aa3e4f99b8efd9407b8b0bbea9adeddc38382
David Son <david@Davids-MacBook-Air.local>
2022-11-24 21:15:55 -0800

    signup connected with db

--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const mainController = require("../controllers/main");
 
 router.get("/", mainController.homePage);
+router.get("/login", mainController.login);
+router.post("/signup", mainController.signup);
 
 module.exports = router;
